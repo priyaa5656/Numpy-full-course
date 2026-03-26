6c3a9f77cd3fe1df12105b60ad71b61210a2c970
priyaa5656 <priyaa555666@gmail.com>
2026-03-26 12:58:41 +0530

    update .py

---
+++ attributes/code.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Array making
+arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+print("Array:\n", arr1)
+
+# Shape
+print("Shape:", arr1.shape)
+
+# Size
+print("Size:", arr1.size)
+
+# Data Type
+print("dtype:", arr1.dtype)
+
+# Number of Dimensions
+print("ndim:", arr1.ndim)
